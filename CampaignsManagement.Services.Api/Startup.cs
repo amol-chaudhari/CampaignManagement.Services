f14182b336e4c98eb0949e5c93f767354051bc11
Amol Chaudhari <37899236+amol-chaudhari@users.noreply.github.com>
2019-07-02 15:56:38 +0200

    V1
    Update cors implementation.

--- CampaignsManagement.Services.Api/Startup.cs
+++ CampaignsManagement.Services.Api/Startup.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Owin;
+using Microsoft.Owin;
 using Microsoft.Owin.Cors;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
 using System;
+using System.Threading.Tasks;
+using System.Web.Cors;
 
 [assembly: OwinStartupAttribute(typeof(CampaignsManagement.Services.Api.Startup))]
 namespace CampaignsManagement.Services.Api
@@ -12,18 +14,36 @@ namespace CampaignsManagement.Services.Api
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigureCors(app);
 
-            app.UseCors(CorsOptions.AllowAll);
+            //app.UseCors(CorsOptions.AllowAll);
 
             OAuthAuthorizationServerOptions option = new OAuthAuthorizationServerOptions
             {
+                AllowInsecureHttp = true,
                 TokenEndpointPath = new PathString("/token"),
-                Provider = new ApplicationOAuthProvider(),
                 AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(60),
-                AllowInsecureHttp = true
+                Provider = new ApplicationOAuthProvider()
             };
             app.UseOAuthAuthorizationServer(option);
             app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+
+        public void ConfigureCors(IAppBuilder app)
+        {
+            var corsPolicy = new CorsPolicy
+            {
+                AllowAnyMethod = true,
+                AllowAnyHeader = true,
+                AllowAnyOrigin = true
+            };
+
+            var corsOptions = new CorsOptions
+            {
+                PolicyProvider = new CorsPolicyProvider { PolicyResolver = context => Task.FromResult(corsPolicy) }
+            };
+
+            app.UseCors(corsOptions);
         }
     }
 }
